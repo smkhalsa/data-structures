bc6903b8f85b626772e93c99737a3afdfbdf4ff9
smkhalsa <smkhalsa@gmail.com>
2015-05-14 11:28:01 -0700

    All cases of sprint one complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.count = 0;
+  obj.storage = {};
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function(){
+    var result = this.storage[0];
+    var reStore = {};
+    if(this.count>0) {
+      delete this.storage[0];
+      _.each(this.storage, function(value, key){
+        reStore[key-1] = value;
+      });
+      this.storage = reStore;
+      this.count--;
+    }
+    return result;
+  },
+  size: function(){
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  var result = this.storage[0];
+  var tempStorage = {};
+
+  if(this.count > 0) {
+    delete this.storage[0];
+    _.each(this.storage, function(value, key){
+      tempStorage[key-1] = value;
+    });
+    this.count--;
+    this.storage = tempStorage;
+  }
+  return result;
+};
+
+Queue.prototype.size = function () {
+  return this.count;
+};
 
