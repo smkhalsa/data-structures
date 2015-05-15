b5a45def833f3da040ba93083cbd4ea17a6cd588
smkhalsa <smkhalsa@gmail.com>
2015-05-14 20:40:05 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // create a new node with the argument value
+    // set previous node's next property to new node
+    // set tail property to new node
+
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
+    else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    // set head to next node
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target){
+    var result = false;
+    // start at head
+    // check if head is target
+    if(list.head === null) {
+      return result;
+    }
+    var nodeCheck = function(node) {
+      if(node.value === target) {
+        result = true;
+      } else {
+        if(node.next) {
+          nodeCheck(node.next);
+        }
+      }
+    };
+    nodeCheck(list.head);
+    return result;
   };
 
   return list;
