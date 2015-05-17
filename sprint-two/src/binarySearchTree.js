c0b71c95fe5b7e414f29f577a0217b62fd3c44e1
smkhalsa <smkhalsa@gmail.com>
2015-05-16 18:00:29 -0700

    Completed binarySearchTree and cleaned up hashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,52 @@
 var BinarySearchTree = function(value){
+  var newTree = Object.create(BinarySearchTree.prototype);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
+};
+
+BinarySearchTree.prototype.insert =  function(node) {
+  var direction;
+  node > this.value ? direction = "right" : direction = "left";
+  if(!this[direction]) {
+    this[direction] = BinarySearchTree(node);
+  } else {
+    this[direction].insert(node);
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function(target){
+  var targetFound = false;
+  var searchTree = function(node) {
+    var direction;
+    if(node.value === target) {
+      targetFound = true;
+    } else if (node.value < target) {
+      direction = "right";
+    } else if (node.value > target) {
+      direction = "left";
+    }
+    if(node[direction]) {
+      searchTree(node[direction]);
+    }
+  };
+  searchTree(this);
+  return targetFound;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  var runCallBack = function (node) {
+    callback(node.value);
+    if (node.left) {
+      runCallBack(node.left);
+    }
+    if (node.right) {
+      runCallBack(node.right);
+    }
+  };
+  runCallBack(this);
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,57 +5,37 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
 
-  // create two-ple combining key and value (key will always be at [0])
   var twoPle = [k, v];
-  // pass in key to hashing function to produce the bucketIndex
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // if storage at bucket index is undefined
   if(!this._storage[i]) {
-    // create empty array at bucketIndex of storage
     this._storage[i] = [];
   }
-  // set temp variable to bucketIndex of storage
   var temp = this._storage[i];
-  // push two-ple into temp
   temp.push(twoPle);
-  // set bucketIndex of storage to temp
   this._storage[i] = temp;
 };
 
 HashTable.prototype.retrieve = function(k){
-  // define result variable
   var result = null;
-  // pass in key to hashing function to get bucketIndex
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // set new var currentBucket to storage at bucketIndex
   var currentBucket = this._storage[i];
-  // iterate over currentBucket
   for (var i=0; i<currentBucket.length; i++) {
-    // if first item of two-ple matches key
     if (currentBucket[i][0] === k) {
-      // set result to second item of two-ple
       result = currentBucket[i][1];
     }
   }
-  // return result variable
   return result;
 
 };
 
 HashTable.prototype.remove = function(k){
-  // set variable i to result of running hashing fn on key
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // create variable currentBucket and set it to storage at index i
   var currentBucket = this._storage[i];
-  // iterate over currentBucket
   for (var i=0; i<currentBucket.length; i++) {
-    // if first item of current twoPle is equal to key
     if (currentBucket[i][0] === k) {
-      // remove twoPle from currentBucket
       currentBucket.splice(i, 1);
     }
   }
-  // set storage at index i to currentBucket
   this._storage[i] = currentBucket;
 };
 
